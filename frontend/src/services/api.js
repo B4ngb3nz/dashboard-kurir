fa27408853567be3e2819fb4325d1777ddb59b88
B4ngb3nz <abay30@gmail.com>
2026-05-11 18:52:11 +0700

    render deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API =
-  "http://localhost:3001/api/dashboard";
+ https://dashboard-api.onrender.com/api
+ // "http://localhost:3001/api/dashboard";
 
 export const getDashboard =
   async (params) => {
